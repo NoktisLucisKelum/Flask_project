cfe1b2e9d450880a57435ec14c74fe66c654036a
NoktisLucisKelum <55601448+NoktisLucisKelum@users.noreply.github.com>
2025-07-13 15:54:43 +0300

    Add files via upload

---
+++ users.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    telefon = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, nullable=True)
+    surgeter = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, nullable=True)
+    cv = sqlalchemy.Column(sqlalchemy.Integer,
+                           index=True, nullable=True)
+    kindofcook = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    amount = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    comment = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    time = sqlalchemy.Column(sqlalchemy.String, nullable=True)
